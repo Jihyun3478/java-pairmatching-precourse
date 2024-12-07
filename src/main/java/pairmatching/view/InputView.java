2b647bcf4f000b8b31424d36ffe5b1605be57e0b
Jihyun3478 <leejihyun3478@gmail.com>
2024-12-07 16:41:15 +0900

    feat: OutputView, InputView 작성

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,13 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String menu() {
+        return Console.readLine();
+    }
+
+    public static String step() {
+        return Console.readLine();
+    }
+}
